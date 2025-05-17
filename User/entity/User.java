f6502491af7ff6ad5403a1a95801d8cc05cfc31e
문이현 (2176134) <113423515+dlgus0919@users.noreply.github.com>
2025-05-17 22:41:32 +0900

    Create User.java

---
+++ User/entity/User.java
@@ -0,0 +1,23 @@
+package fooding.foodingback.User.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String kakaoId;
+
+    private String email;
+    private String nickname;
+    private String profileImage;
+}
