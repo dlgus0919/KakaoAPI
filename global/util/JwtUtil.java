440e139f4dd5c33379aef45acecffebd743ad90f
문이현 (2176134) <113423515+dlgus0919@users.noreply.github.com>
2025-05-17 22:40:06 +0900

    Create JwtUtil.java

---
+++ global/util/JwtUtil.java
@@ -0,0 +1,25 @@
+package foodingback.global.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
+
+    public String generateToken(String kakaoId) {
+        return Jwts.builder()
+                .setSubject(kakaoId)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
